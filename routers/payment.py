aa636fdeb7ab600bc09931fcf79b4231bac59521
prathapkr <your_email@example.com>
2025-01-14 16:32:17 +0530

    price updates to 39

--- routers/payment.py
+++ routers/payment.py
@@ -69,7 +69,7 @@ async def create_checkout_session(
                 'price_data': {
                     'currency': 'usd',
                     'product': STRIPE_PRODUCT_ID,
-                    'unit_amount': 100,  # $49.00
+                    'unit_amount': 39000,  # $49.00
                 },
                 'quantity': 1,
             }],
